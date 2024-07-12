74ff304afbf0cf005b864319feb9c6dc90d35392
XiuzhuJian_05qqcw2 <xiuzhu.jian@vayyar.com>
2024-07-12 10:01:33 +0800

    chore: change vayyar care config

--- Config.py
+++ Config.py
@@ -1,15 +1,15 @@
 class Config(object):
     def __init__(self):
-        self.d455_x = 0.568
+        self.d455_x = 0
         self.d455_y = 0
-        self.d455_z = 1.207
+        self.d455_z = 0.78
 
-        self.x_min = -2
-        self.x_max = 2
+        self.x_min = -1.5
+        self.x_max = 1.5
         self.y_min = 0.2
-        self.y_max = 4
+        self.y_max = 3
         self.z_min = 0
-        self.z_max = 2
+        self.z_max = 1.8
 
 
 config = Config()
