2dceffcb7ae4eb4b41141536881184d979ca757f
XiuzhuJian_05qqcw2 <xiuzhu.jian@vayyar.com>
2024-12-18 22:20:00 +0800

    feat: Implement skeleton points from L515 coord system to VC coord system.

--- CoordConverter.py
+++ CoordConverter.py
@@ -1,3 +1,7 @@
+from L515Coord import *
+from PostureType import *
+from VCCoord import *
+
 X = 0
 Y = 1
 Z = 2
@@ -16,8 +20,33 @@ POS_TO_L515_POS_TYPE = {
 }
 
 
-def convert_skeleton_to_vc_coord_system(skeleton_coords: list, vc_id:int):
-    pass
+def convert_skeleton_to_vc_coord_system(skeleton_coords: list, pos_idx: int, vc_id: int, posture_type: PostureType):
+    """
+    skeleton L515 -> world -> vc
+    """
+    if posture_type == PostureType.STAND:
+        l515_coord = stand_pos_idx_to_l515_coord(pos_idx)
+    elif posture_type == PostureType.SIT:
+        l515_coord = sit_pos_idx_to_l515_coord(pos_idx)
+    else:
+        print('posture_type must be STAND, SIT')
+        return
+
+    # Determine the appropriate converter based on the position of the L515.
+    if posture_type == PostureType.STAND and pos_idx_to_row_idx(pos_idx) in (0, 1):
+        converter = front_l515_to_world
+    else:
+        converter = back_l515_to_world
+
+    # convert from L515 coord system to world coord system
+    skeleton_vc_coords = []
+    vc_world_coord = VC_ID_TO_COORD[vc_id]
+    for skeleton_coord in skeleton_coords:
+        skeleton_world_coord = converter(skeleton_coord, l515_coord)
+        skeleton_vc_coord = world_to_vc(skeleton_world_coord, vc_world_coord)
+        skeleton_vc_coords.append(skeleton_vc_coord)
+
+    return skeleton_vc_coords
 
 
 def front_l515_to_world(l515_point_coord, l515_world_coord):
---
+++ PostureType.py
@@ -0,0 +1,6 @@
+import enum
+
+
+class PostureType(enum.Enum):
+    STAND = enum.auto()
+    SIT = enum.auto()
