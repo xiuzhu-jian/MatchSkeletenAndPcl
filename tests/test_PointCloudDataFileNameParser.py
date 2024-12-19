92589eadfce2296840bb6192a2d497a1959de9b4
XiuzhuJian_05qqcw2 <xiuzhu.jian@vayyar.com>
2024-12-19 13:26:53 +0800

    refactor: Improve readability.

--- tests/test_PointCloudDataFileNameParser.py
+++ tests/test_PointCloudDataFileNameParser.py
@@ -1,11 +1,11 @@
 from PointCloudDataFileNameParser import *
 
 
-def test_get_arena():
+def test_parse_arena():
     assert parse_arena('stand_stand_-2.00_2.00_-2.00_2.00_0.00_2.00_2.45_2.00_1733984702.6237457.txt') == [
         -2.0, 2.0, -2.0, 2.0, 0.0, 2.0
     ]
 
 
-def test_get_height():
+def test_parse_height():
     assert parse_height('stand_stand_-2.00_2.00_-2.00_2.00_0.00_2.00_2.45_2.00_1733984702.6237457.txt') == 2.45
