3d1b2311a88cf95af1dcdb40c32857254338f5d3
XiuzhuJian_05qqcw2 <xiuzhu.jian@vayyar.com>
2024-12-19 13:26:13 +0800

    refactor: Improve readability.

--- PointCloudDataFileNameParser.py
+++ PointCloudDataFileNameParser.py
@@ -1,5 +1,5 @@
-def get_arena(point_cloud_data_filename: str):
+def parse_arena(point_cloud_data_filename: str):
     return list(map(float, point_cloud_data_filename.split('_')[2:8]))
 
-def get_height(point_cloud_data_filename: str):
+def parse_height(point_cloud_data_filename: str):
     return float(point_cloud_data_filename.split('_')[8])
